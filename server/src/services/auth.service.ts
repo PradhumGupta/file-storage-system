e0e32fe8ca0f7b69d605cc52a4db4f28f626ee8d
Pradhum Gupta <guptapradhum77@gmail.com>
2026-06-08 15:56:48 +0530

    oauth service added

--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -4,9 +4,10 @@ import bcrypt from "bcryptjs";
 import { WorkspaceServices } from "./workspace.service";
 // import { emailQueue } from "../queues/email.queue";
 import { generateAccessToken, generateRefreshToken } from "../utils/jwt";
+import { OAuth2Client } from "google-auth-library";
 
 const {ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET} = process.env;
-
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 export class AuthService {
     public register = async (name: string, email: string, password: string) => {
         const existing = await prisma.user.findUnique({ where: { email } });
@@ -39,7 +40,7 @@ export class AuthService {
         await prisma.user.update({
             where: { id: user.id },
             data: { refreshToken }
-        });  // user object not updated before returning but correctly updated on db
+        });
 
         return { accessToken, refreshToken, user };
     }
@@ -76,5 +77,37 @@ export class AuthService {
         const users = await prisma.user.findMany();
 
         return users;
+    }
+
+    public verifyGoogleSubID = async (idToken: string) => {
+        const ticket = await client.verifyIdToken({
+            idToken,
+            audience: process.env.GOOGLE_CLIENT_ID
+        })
+
+        const payload = ticket.getPayload();
+        if (!payload || !payload.email) throw new Error("Invalid Google token");
+
+        const { email, name, sub } = payload;
+        
+        let user = await prisma.user.findUnique({ where: { email } });
+
+        if (!user) {
+            const hashed = await bcrypt.hash(sub + process.env.ACCESS_TOKEN_SECRET, 10);
+            user = await prisma.user.create({
+                data: { name: name || "Google User", email, password: hashed },
+            });
+            await WorkspaceServices.createPersonalWorkspace(user.name, user.id);
+        }
+
+        const accessToken = generateAccessToken(user.id);
+        const refreshToken = generateRefreshToken(user.id);
+
+        await prisma.user.update({
+            where: { id: user.id },
+            data: { refreshToken }
+        });
+
+        return { accessToken, refreshToken, user };
     }
 }
